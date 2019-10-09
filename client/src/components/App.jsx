1e6a5154920b78b1e32e4c2784bb2f181acc1762
Brian Kim <briaansjkim@gmail.com>
2019-10-08 20:01:46 -0400

    init

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
-  };
+  }
 
   render() {
     return (
       <div>Hello</div>
     );
   }
-};
+}
 
 export default App;
