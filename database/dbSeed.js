8b0d0f72879611eeaed8decfb04a6315d09bdb95
Brian Kim <briaansjkim@gmail.com>
2019-10-10 14:58:38 -0400

    correctly implemented seed function and used unique id

--- database/dbSeed.js
+++ database/dbSeed.js
@@ -1,26 +1,27 @@
 // this is where the photos will be saved/fetched from the db
 
 // this is the model
-const Photo = require('./index.js');
+const model = require('./index.js');
 
-const save = (req, res, cb) => {
+const save = () => {
   const photos = [
     {
       'id': 1,
       'img_url': 'https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg',
       'description': 'A cow looking at the camera',
       'title': 'Cows',
-      'people': 'One Cow'
+      'people': 'Cow'
     }
   ];
 
   for (photo of photos) {
-    var newPhoto = new Photo(photo);
+    var newPhoto = new model.Photo(photo);
     newPhoto.save();
   }
 
   console.log('Database seeded');
 };
 
+save()
 
 module.exports.save = save;
