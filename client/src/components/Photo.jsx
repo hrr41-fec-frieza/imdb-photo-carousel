accdf40c90c54298c23f5d3e961c90fdd7733a6f
Brian Kim <briaansjkim@gmail.com>
2019-10-11 17:52:06 -0400

    configured images into modal images

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -6,10 +6,50 @@ const Img = styled.img`
   width: 200px;
 `;
 
-const Photo = ({img}) => (
-  <div>
-    <Img src={img} />
-  </div>
-);
+const BigImg = styled.img`
+  height: 300px;
+  width: 400px;
+`
+// const Photo = ({img}) => (
+//   <div>
+//     <Img src={img} />
+//   </div>
+// );
+
+class Photo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+
+    this.expandPic = this.expandPic.bind(this);
+  }
+
+  expandPic() {
+    // console.log('Works');
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    const isOpen = this.state.isOpen;
+    let pic;
+
+    if (isOpen) {
+      pic = <Img onClick={this.expandPic} src={this.props.img} />
+    } else {
+      pic = <BigImg onClick={this.expandPic} src={this.props.img} />
+    }
+
+    return (
+      <div>
+        {pic}
+      </div>
+    )
+  }
+}
 
 export default Photo;
