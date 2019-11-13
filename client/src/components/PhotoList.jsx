be485f0204dfcb33825dd68049c1162e80240f22
Brian Kim <briaansjkim@gmail.com>
2019-11-13 17:59:41 -0500

    no changes

--- client/src/components/PhotoList.jsx
+++ client/src/components/PhotoList.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import styled from 'styled-components';
 import Photo from './Photo.jsx';
 
@@ -10,13 +9,6 @@ const BDiv = styled.div`
 
 const PhotoList = ({ photos }) => (
   <BDiv>
-=======
-import styles from './css/PhotoList.css';
-import Photo from './Photo.jsx';
-
-const PhotoList = ({ photos }) => (
-  <div className={styles.div}>
->>>>>>> 64d665d37cda8f6d27abb2dd04152902d6b8bba5
     {photos.map((photo, index) =>
       <Photo photo={photo} key={index} />
     )}
