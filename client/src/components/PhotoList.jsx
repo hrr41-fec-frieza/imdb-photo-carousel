0a8d872267ce4c98b7e93b161caa9204fe30b879
Brian Kim <briaansjkim@gmail.com>
2019-11-13 18:52:53 -0500

    no changes

--- client/src/components/PhotoList.jsx
+++ client/src/components/PhotoList.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import styles from './css/PhotoList.css';
 import Photo from './Photo.jsx';
 
-const BDiv = styled.div`
-  display: flex;
-  flex-direction: row
-`;
 
 const PhotoList = ({ photos }) => (
   <div className={styles.div}>
